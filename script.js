e6888f5f6c54a01c07ce0b02eecd186a53fac46b
unknown <irandironofre5@gmail.com>
2020-10-02 11:18:31 -0300

    primeiro coment

---
+++ script.js
@@ -0,0 +1,4 @@
+//C = 5 * ((F-32) / 9).
+let celsius = parseFloat(prompt("Digite a temperatura em Celsius"));
+let fahrenheit =  celsius/5 * 9+32;
+alert("A temperatura em fahrenheit: "+fahrenheit)  
